48683ec48c830350db81d416551175a29ce2a45d
DadaCap <danlesupergameuryt2@gmail.com>
2024-04-22 14:08:53 +0200

    Added Ruby Item (Not Working)

--- src/main/java/net/dadacap/dungeonmob/DungeonMob.java
+++ src/main/java/net/dadacap/dungeonmob/DungeonMob.java
@@ -1,16 +1,16 @@
 package net.dadacap.dungeonmob;
 
+import net.dadacap.dungeonmob.item.ModItems;
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DungeonMob implements ModInitializer {
 	public static final String MOD_ID = "dungeonmob";
-    public static final Logger LOGGER = LoggerFactory.getLogger("dungeonmob");
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
